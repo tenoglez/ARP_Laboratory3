43757f9bad29a103d4fe53df8bc64d2bc38c7913
pepelu8000 <joselu_1993@hotmail.es>
2016-09-14 00:02:25 +0200

    Include and comment dataset wiki_graph

---
+++ lab3package/R/wiki_graph.R
@@ -0,0 +1,13 @@
+#' Example graph information
+#'
+#' A dataset containing the nodes and edges of an example graph
+#'
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{vector of nodes where edges start}
+#'   \item{v2}{vector of nodes where edges end}
+#'   \item{w}{weight of the edges}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstras_algorithm}
+"wiki_graph"
