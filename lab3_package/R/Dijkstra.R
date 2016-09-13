2367ed771ddd24851f6cbb253ac2f9eaad938a91
ejarkm <ejarkm@gmail.com>
2016-09-13 23:18:48 +0200

    Euclidean and Dijkstra algorithms commented

--- lab3_package/R/Dijkstra.R
+++ lab3_package/R/Dijkstra.R
@@ -1,4 +1,25 @@
-
+#' Dijkstra algorithm
+#' 
+#' @description 
+#' Algorithm that takes a graph and an initial node and calculates the shortest path from the initial node to every other node in the graph.
+#' 
+#' 
+#' @param graph A \code{data.frame} with three columns that describes the graph: \code{v1} (nodes where edges start), \code{v2} (nodes where edges end) and \code{w} (weigth of the edges)
+#' @param init_node A positive scalar number for the initial node
+#' 
+#' @return \code{vector} with the shortest path from the initial node to every other node
+#' 
+#' @references 
+#' 
+#' Dijkstra algorithm - https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+#' 
+#' @examples
+#' wiki_graph <- data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+#'          v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+#'           w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+#' 
+#' dijkstra(wiki_graph, 3)
+#' 
 dijkstra <- function(graph, init_node) {
   # stop in case graph is not a data frame with numeric columns (v1, v2, w) and init_node is a numeric scalar
   stopifnot(is.data.frame(graph), is.numeric(init_node), colnames(graph) == c("v1", "v2", "w"), sapply(wiki_graph, is.numeric))
--- lab3_package/R/Euclidean.R
+++ lab3_package/R/Euclidean.R
@@ -1,4 +1,22 @@
-#Recursive Euclidean function
+#' Recursive Euclidean algorithm function
+#' 
+#' @description 
+#' Algorithm to find the greatest common divisor of two numbers
+#' 
+#' 
+#' @param a A positive scalar number
+#' @param b A positive scalar number
+#' 
+#' @return The maximum common divisor of the two numbers
+#' 
+#' @references 
+#' 
+#' Euclidean algorithm - https://en.wikipedia.org/wiki/Euclidean_algorithm
+#' 
+#' @examples
+#' euclidean(93164, 5826)
+#' euclidean(1000, 250)
+#' 
 euclidean <- function(a, b){
   if (b == 0){
     return (a) 
